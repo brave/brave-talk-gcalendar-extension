678db7204c38a4d214fc83d54cfc836846d9d8f4
Graham Tackley <gtackley@brave.com>
2022-01-11 11:29:26 +0000

    Tests for the full screen edit mode
    These aren't ideal as it's based on html extracted from gcal. But better than nothing, and provides a framework for diagnosing issues going forward. Due to the very dynamic nature of gcal, mnost likely issues will need to be reproduced "live" but then can be incorporated into this framework to avoid future regression.

--- src/google-calendar.test.ts
+++ src/google-calendar.test.ts
@@ -1,9 +1,17 @@
 import * as gcal from "./google-calendar";
 import $ from "jquery";
+import fs from "fs";
+import path from "path";
 
-const waitForMutationObserversToFire = (): Promise<void> => {
+function waitForMutationObserversToFire(): Promise<void> {
   return new Promise((resolve) => process.nextTick(resolve));
-};
+}
+
+function loadTestDoc(filename: string): string {
+  return fs.readFileSync(path.join(__dirname, "testdata", filename), {
+    encoding: "utf-8",
+  });
+}
 
 it("should correctly detect whether gcal has loaded", () => {
   document.body.outerHTML =
@@ -56,4 +64,42 @@ it("should add button to quick add dialog", async () => {
   expect(document.getElementById("jitsi_button_quick_add")).not.toBeNull();
 });
 
-it("should add 'Add Brave Talk' button to full screen event edit if location is not currently brave talk", () => {});
+it("should add 'Add Brave Talk' button to full screen event edit if location is not currently brave talk", async () => {
+  document.body.outerHTML = '<body data-viewfamily="EVENT_EDIT"></body>';
+
+  const newEventHtml = loadTestDoc("new_event.html");
+  gcal.watchForChanges();
+
+  document.body.innerHTML = newEventHtml;
+
+  // give the mutation observer a chance to run
+  await waitForMutationObserversToFire();
+
+  const el = document.getElementById("jitsi_button");
+  expect(el).not.toBeNull();
+
+  const link = el?.getElementsByTagName("a")[0];
+  expect(link?.outerHTML).toMatchInlineSnapshot(
+    `"<a href=\\"#\\" style=\\"color: white\\">Add a Brave Talk meeting</a>"`
+  );
+});
+
+it("should add 'Join Brave Talk' button to full screen event edit if location is currently brave talk", async () => {
+  document.body.outerHTML = '<body data-viewfamily="EVENT_EDIT"></body>';
+
+  const newEventHtml = loadTestDoc("event_with_talk_meeting_location.html");
+  gcal.watchForChanges();
+
+  document.body.innerHTML = newEventHtml;
+
+  // give the mutation observer a chance to run
+  await waitForMutationObserversToFire();
+
+  const el = document.getElementById("jitsi_button");
+  expect(el).not.toBeNull();
+
+  const link = el?.getElementsByTagName("a")[0];
+  expect(link?.outerHTML).toMatchInlineSnapshot(
+    `"<a href=\\"https://talk.brave.com/yzd4Zi1B5Gf48m4X_TsNrDZSmTCdIhAvJ10Wz5gh4FA\\" style=\\"color: white\\" target=\\"_new\\">Join your Brave Talk meeting now</a>"`
+  );
+});
